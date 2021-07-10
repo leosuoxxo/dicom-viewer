257a647c613060467375832fdaf7d8d82f697dd5
RogerHuang <ziwei443@gmail.com>
2021-07-10 14:23:10 +0800

    Part 5. ToolBar typo

--- src/containers/ToolBar/ToolBar.js
+++ src/containers/ToolBar/ToolBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import {
+    Build,
+    ExitToApp,
+} from '@material-ui/icons';
 
+import { Flex } from '../../components/elements';
 import FileUploadTool from './FileUploadTool';
 import LengthTool from './LengthTool';
 
@@ -8,8 +13,20 @@ export const ToolBar = () => {
   return (
     <AppBar>
       <Toolbar>
+        {/*Frequently used tools*/}
         <FileUploadTool />
         <LengthTool />
+        <Flex style={{ flexGrow: 1 }} />
+
+        {/*General function*/}
+        {/*TBD: list all of supported tools*/}
+        <IconButton>
+          <Build />
+        </IconButton>
+        {/*TBD: licence related*/}
+        <IconButton>
+          <ExitToApp />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
