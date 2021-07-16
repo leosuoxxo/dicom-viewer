d9db89c165b1f0e58f1d296d6ceb9db93de34c4e
Leo <grong.su@gmail.com>
2021-07-17 02:31:37 +0800

    style: lint style format

--- src/services/canvasToTiffService.js
+++ src/services/canvasToTiffService.js
@@ -1,9 +1,9 @@
 export const useCanvasToTiffService = () => {
-  const toTiffUrl = canvas => {
+  const toTiffUrl = (canvas) => {
     return new Promise((resolve) => {
-        window.CanvasToTIFF.toObjectURL(canvas, url => {
-            resolve(url);
-        });
+      window.CanvasToTIFF.toObjectURL(canvas, (url) => {
+        resolve(url);
+      });
     });
   };
   return { toTiffUrl };
