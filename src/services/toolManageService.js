ab03a6a68cb7e63e32f86126fe360b864b9b565d
Jamie Tu <jd3main@gmail.com>
2021-08-17 17:07:14 +0800

    Move DICODE format conversion to utils

--- src/services/toolManageService.js
+++ src/services/toolManageService.js
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import Tiff from 'tiff.js';
 import { useCornerstone } from './cornerstoneService';
 import { useCanvasToTiffService } from './canvasToTiffService';
-import { getFileExtension, fileToBuffer } from '../utils';
+import { getFileExtension, fileToBuffer, convertDiconde } from '../utils';
 import { IMAGE_TYPE } from '../constants';
-import dicomParser from 'dicom-parser';
 
 export const useToolManageService = () => {
   const {
@@ -38,76 +37,9 @@ export const useToolManageService = () => {
         case 'diconde': {
           const buffer = await fileToBuffer(file);
           const bytes = new Uint8Array(buffer);
-          const dicomData = dicomParser.parseDicom(bytes);
-          const modifiedBytes = new Uint8Array(bytes.length);
-
-          // preamble and prefixing 'DICM'
-          const preableLength = 128;
-          const dicomPrefix = 'DICM';
-          let totalPrefixLength = preableLength + dicomPrefix.length;
-          modifiedBytes.set(bytes.subarray(0, totalPrefixLength), 0);
-
-          // data elements
-          let rIndex = totalPrefixLength;
-          let wIndex = totalPrefixLength;
-          while (rIndex < bytes.length) {
-            const tag = new Uint16Array(bytes.slice(rIndex, rIndex + 4).buffer);
-            const tagStr =
-              'x' +
-              tag[0].toString(16).padStart(4, '0') +
-              tag[1].toString(16).padStart(4, '0');
-
-            const element = dicomData.elements[tagStr];
-            const dataBegin = element.dataOffset;
-            const dataEnd = dataBegin + element.length;
-            const infoLength = dataBegin - rIndex;
-
-            switch (tagStr) {
-              case 'x00281050':
-              case 'x00281051': {
-                console.log(`Ignore tag: ${tagStr}`);
-                break;
-              }
-              case 'x00280008': {
-                // write Tag and VR
-                modifiedBytes.set(bytes.subarray(rIndex, rIndex + 6), wIndex);
-                wIndex += 6;
-                // write Value Length
-                modifiedBytes.set([1, 0], wIndex);
-                wIndex += 2;
-                // write Value Field
-                modifiedBytes.set(['1'.charCodeAt()], wIndex);
-                wIndex += 1;
-                break;
-              }
-              case 'x00280002': {
-                // write Tag and VR
-                modifiedBytes.set(bytes.subarray(rIndex, rIndex + 6), wIndex);
-                wIndex += 6;
-                // write Value Length
-                modifiedBytes.set([2, 0], wIndex);
-                wIndex += 2;
-                // write Value Field
-                modifiedBytes.set([1, 0], wIndex);
-                wIndex += 2;
-                break;
-              }
-              default: {
-                // write Tag, VR, and Value Length
-                modifiedBytes.set(bytes.subarray(rIndex, dataEnd), wIndex);
-                wIndex += infoLength;
-                // write Value Field
-                modifiedBytes.set(bytes.subarray(dataBegin, dataEnd), wIndex);
-                wIndex += element.length;
-              }
-            }
-
-            rIndex = dataEnd;
-          }
-
-          const actualWrittenBuffer = modifiedBytes.slice(0, wIndex).buffer;
+          const convertedBytes = convertDiconde(bytes);
           const convertedFile = new File(
-            [actualWrittenBuffer],
+            [convertedBytes.buffer],
             `converted_${file.name}`
           );
           const imageId =
