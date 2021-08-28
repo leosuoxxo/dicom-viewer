bc21987699969dfadf5e54ba8f9ce34fadda4691
RogerHuang <ziwei443@gmail.com>
2021-08-28 16:02:00 +0800

    Part 5. integrate annotateTool

--- src/components/AnnotateTool/index.js
+++ src/components/AnnotateTool/index.js
@@ -1,13 +1,4 @@
 export { Tool as default } from './Tool';
-
-export { EraserTool } from './EraserTool';
 export { HandTool } from './HandTool';
-export { AngleTool } from './AngleTool';
-export { FreeHandRoiTool } from './FreeHandRoiTool';
-export { LengthTool } from './LengthTool';
-export { RectangleRoiTool } from './RectangleRoiTool';
-export { EllipticalRoiTool } from './EllipticalRoiTool';
-export { ProbeTool } from './ProbeTool';
-export { ArrowAnnotateTool } from './ArrowAnnotateTool';
 
 export { ToolMenu } from './ToolMenu';
