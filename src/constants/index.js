beed1e4d3dea878a38f559a88c4a5dece7a16062
RogerHuang <ziwei443@gmail.com>
2021-09-08 11:47:02 +0800

    part 5. move constants

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const IMAGE_TYPE = {
+  PNG: 'png',
+  JPG: 'jpeg',
+  TIFF: 'tiff',
+  TIF: 'tif',
+};
+export const VIEWER_LAYOUT = ['1*1', '1*2'];
+export const TOOLBAR_HEIGHT = '64px';
+export const TOOL_COLORS = ['deepSkyBlue'];
+
+export { UIDS } from './uids';
+export { DCM_TAGS, SPECIFIC_SERIAL } from './dcmTags';
